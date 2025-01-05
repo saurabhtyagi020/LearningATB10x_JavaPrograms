422a73aa05882383d151ed014929ac07aec7af13
HP <saurabhtyagi020@gmail.com>
2025-01-05 21:07:33 +0530

    Basis program 1

---
+++ src/Ex_01_Java_Basic/Lab004_Addition.java
@@ -0,0 +1,10 @@
+package Ex_01_Java_Basic;
+
+public class Lab004_Addition {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("Additon= "+c);
+    }
+}
