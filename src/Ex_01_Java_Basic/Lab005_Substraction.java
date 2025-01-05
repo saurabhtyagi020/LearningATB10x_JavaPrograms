372ecfa506b014c2736caf76d25e05f771ae5129
HP <saurabhtyagi020@gmail.com>
2025-01-05 21:10:18 +0530

    New commit

---
+++ src/Ex_01_Java_Basic/Lab005_Substraction.java
@@ -0,0 +1,12 @@
+package Ex_01_Java_Basic;
+
+public class Lab005_Substraction {
+
+    public static void main(String[] args) {
+        int a=40;
+        int b=15;
+        int c=a-b;
+
+        System.out.println("Substracton= "+c);
+    }
+}
