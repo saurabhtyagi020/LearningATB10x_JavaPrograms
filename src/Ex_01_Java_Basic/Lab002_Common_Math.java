d30ba8cd7162dca00bec84d8b1f4e0efaf321433
HP <saurabhtyagi020@gmail.com>
2025-01-05 21:03:34 +0530

    Basis program

---
+++ src/Ex_01_Java_Basic/Lab002_Common_Math.java
@@ -0,0 +1,10 @@
+package Ex_01_Java_Basic;
+
+public class Lab002_Common_Math {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2*2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+    }
+}
---
+++ src/Ex_01_Java_Basic/Lab003_Print_vs_Println.java
@@ -0,0 +1,12 @@
+package Ex_01_Java_Basic;
+
+import javax.sound.midi.Soundbank;
+
+public class Lab003_Print_vs_Println {
+
+    public static void main(String[] args) {
+        System.out.println("Hey Everyone!");
+        System.out.print("hello");
+        System.out.print("hello hellos");
+    }
+}
