38a826e708077309ee8e140d02e2c6ebbee8df97
HP <saurabhtyagi020@gmail.com>
2025-01-20 22:13:58 +0530

    QnA3

---
+++ src/Ex_07_IncreD/Lab_058_QnA.java
@@ -0,0 +1,13 @@
+package Ex_07_IncreD;
+
+public class Lab_058_QnA {
+    public static void main(String[] args) {
+        String name =args[0];
+        int age = Integer.parseInt(args[1]);
+        int salary=Integer.parseInt(args[2]);
+        System.out.println("Name = "+name);
+        System.out.println("Age = "+age);
+        System.out.println("Salary = "+salary);
+
+    }
+}
