e708afd97a31a97589f47733e9a0015701a98c25
HP <saurabhtyagi020@gmail.com>
2025-02-05 22:34:49 +0530

    05 feb task

---
+++ src/Task_05_feb/Lab_131_QnA.java
@@ -0,0 +1,11 @@
+package Task_05_feb;
+
+public class Lab_131_QnA {
+    public static void main(String[] args) {
+        //Print the multiplication table of 5
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println("5"+"X"+i+" = "+5*i);
+        }
+    }
+}
