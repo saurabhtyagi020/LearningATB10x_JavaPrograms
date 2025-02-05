01e950709476f6bcb832aa9c12743a41cde75be5
HP <saurabhtyagi020@gmail.com>
2025-02-05 22:31:33 +0530

    05 feb task

---
+++ src/Task_05_feb/Lab_129_QnA.java
@@ -0,0 +1,11 @@
+package Task_05_feb;
+
+public class Lab_129_QnA {
+    public static void main(String[] args) {
+        //Print "Hello" 5 times
+        for (int i=0;i<5;i++)
+        {
+            System.out.println("Hello");
+        }
+    }
+}
---
+++ src/Task_05_feb/Lab_130_QnA.java
@@ -0,0 +1,15 @@
+package Task_05_feb;
+
+public class Lab_130_QnA {
+    public static void main(String[] args) {
+        //Print the sum of first 10 natural numbers
+
+        int add=0;
+        for(int i=1;i<=10;i++)
+        {
+            add=add+i;
+        }
+        System.out.println(add);
+
+    }
+}
