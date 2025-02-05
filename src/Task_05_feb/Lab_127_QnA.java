3984fdb38529811ae325e12ccbf24c23a35f1dae
HP <saurabhtyagi020@gmail.com>
2025-02-05 22:22:31 +0530

    05 feb task

---
+++ src/Task_05_feb/Lab_127_QnA.java
@@ -0,0 +1,14 @@
+package Task_05_feb;
+
+public class Lab_127_QnA {
+    public static void main(String[] args) {
+        //Print numbers from 1 to 10
+
+        int num =10;
+        for(int i=1;i<=num;i++)
+        {
+            System.out.println(i);
+        }
+    }
+
+}
---
+++ src/Task_05_feb/Lab_128_QnA.java
@@ -0,0 +1,20 @@
+package Task_05_feb;
+
+public class Lab_128_QnA {
+    public static void main(String[] args) {
+        //Print even numbers from 1 to 20
+        for(int i=1;i<=20;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+            else
+            {
+                System.out.print("");
+            }
+
+        }
+
+    }
+}
