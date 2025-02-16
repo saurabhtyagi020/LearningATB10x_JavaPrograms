6410a56a25ee29526d67613c7b9411fafd795882
HP <saurabhtyagi020@gmail.com>
2025-02-16 22:53:13 +0530

    16 feb

---
+++ src/Ex_21_Wrapper/Lab_188_W1.java
@@ -0,0 +1,14 @@
+package Ex_21_Wrapper;
+
+public class Lab_188_W1 {
+    public static void main(String[] args) {
+        int a =10;
+        Integer b=a;
+        System.out.println(b);
+        System.out.println(b.MAX_VALUE);
+
+
+
+    }
+
+}
---
+++ src/Ex_21_Wrapper/Lab_189_W2.java
@@ -0,0 +1,11 @@
+package Ex_21_Wrapper;
+
+public class Lab_189_W2 {
+    public static void main(String[] args) {
+        String num = "20";
+
+        Integer i = Integer.parseInt(num);
+        System.out.println(i);
+
+    }
+}
