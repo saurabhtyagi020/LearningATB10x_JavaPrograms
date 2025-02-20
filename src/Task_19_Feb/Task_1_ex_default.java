62d792f1333830a15ae3849d040650b0806de9b1
HP <saurabhtyagi020@gmail.com>
2025-02-20 20:55:03 +0530

    19 feb

---
+++ src/Task_19_Feb/Task_1_ex_default.java
@@ -0,0 +1,15 @@
+package Task_19_Feb;
+
+public class Task_1_ex_default {
+    public static void main(String[] args) {
+        ABC abc = new ABC();
+    }
+}
+
+class ABC
+{
+    ABC()
+    {
+        System.out.println("I am the default constructor");
+    }
+}
