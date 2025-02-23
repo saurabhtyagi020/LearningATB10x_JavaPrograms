16f9f8c53338cdf8ec13c4f1f5def961bb5398af
HP <saurabhtyagi020@gmail.com>
2025-02-23 21:58:45 +0530

    23 Feb

---
+++ src/Ex_23_Collection/Lab_204_LL.java
@@ -0,0 +1,23 @@
+package Ex_23_Collection;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Lab_204_LL {
+    public static void main(String[] args) {
+        List student = new LinkedList();
+        student.add("Vinit");
+        student.add("Sumit");
+        student.add("Vikas");
+        student.add("Rajan");
+
+        System.out.println(student);
+
+        Iterator iterator = student.iterator();
+        while(iterator.hasNext())
+        {
+            System.out.println(iterator.next());
+        }
+}
+}
